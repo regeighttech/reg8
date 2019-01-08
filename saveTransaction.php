5f491c4d2bf181fbce0d29c88b69c83408ed522d
Jose Diaz <jodiaz@csumb.edu>
2019-01-08 00:43:13 -0800

    testing

--- saveTransaction.php
+++ saveTransaction.php
@@ -9,19 +9,19 @@ $dbConn = getConnection($dbPass);
 $sql = "INSERT INTO adminDash (uniscoID, employerTaxID, dateOfTrans, name, ssn, employeeID, amtAccessed, remainingBalance, currPayPerEndDate, netAccess, status) 
 		VALUES (:uniscoID, :employerTaxID, :dateOfTrans, :name, :ssn, :employeeID, :amtAccessed, :remainingBalance, :currPayPerEndDate, :netAccess, :status)";
 
-// $parameters = array();
-// $parameters[':uniscoID'] = "U324";
-// $parameters[':employerTaxID'] = "WM980001";
-// $parameters[':dateOfTrans'] = date("Y/m/d");
-// $parameters[':name'] = "Johnny Smith";
-// $parameters[':ssn'] = "9009";
-// $parameters[':employeeID'] = "10001";
-// $parameters[':amtAccessed'] = $_GET['amt'];
-// $parameters[':remainingBalance'] = $_GET['remainig'];
-// $parameters[':currPayPerEndDate'] = date("Y/m/d");
-// $parameters[':netAccess'] = "50";
-// $parameters[':status'] = $_GET['status'];
-// $stmt = $dbConn->prepare($sql);
+$parameters = array();
+$parameters[':uniscoID'] = "U324";
+$parameters[':employerTaxID'] = "WM980001";
+$parameters[':dateOfTrans'] = date("Y/m/d");
+$parameters[':name'] = "Johnny Smith";
+$parameters[':ssn'] = "9009";
+$parameters[':employeeID'] = "10001";
+$parameters[':amtAccessed'] = $_GET['amt'];
+$parameters[':remainingBalance'] = $_GET['remainig'];
+$parameters[':currPayPerEndDate'] = date("Y/m/d");
+$parameters[':netAccess'] = "50";
+$parameters[':status'] = $_GET['status'];
+$stmt = $dbConn->prepare($sql);
 // $stmt->execute($parameters);
 
 ?>
