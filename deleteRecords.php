02bb3dc051c44cfb565d3654c2b87d71c5156843
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2019-01-14 21:39:39 -0800

    delete button added functionality

--- deleteRecords.php
+++ deleteRecords.php
@@ -10,7 +10,7 @@ $sql = 'DELETE * FROM adminDash WHERE transactionID != 1';
 
 try{
 $stmt = $dbConn->prepare($sql);
-$stmt->execute($parameters);
+$stmt->execute();
 } catch (PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
     }
