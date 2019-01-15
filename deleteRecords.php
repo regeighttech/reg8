6bc013141b2b8e0594de947cb3d4434e68144bbd
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2019-01-14 21:44:07 -0800

    delete button added functionality

--- deleteRecords.php
+++ deleteRecords.php
@@ -6,7 +6,7 @@ date_default_timezone_set('America/Los_Angeles');
 include "databaseConnection.php";
 $dbConn = getConnection();
 
-$sql = 'DELETE * FROM adminDash WHERE transactionID != 1';
+$sql = 'DELETE FROM adminDash WHERE transactionID != 1';
 
 try{
 $stmt = $dbConn->prepare($sql);
