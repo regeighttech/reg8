c81229722e4b0c2721425d9fa451d2cba0220b3b
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2019-01-22 00:16:09 -0600

    Adding siisu logo

--- siisu.php
+++ siisu.php
@@ -52,17 +52,18 @@
 </style>
 </head>
 <body>
-<img src="img/siisu.png" alt="SiiSu Logo">
 
 <video autoplay muted loop id="myVideo">
   <source src="city.mp4" type="video/mp4">
   Your browser does not support HTML5 video.
 </video>
 
+<img src="img/siisu.png" alt="SiiSu Logo">
+
 <div class="content">
   <!--<h1>Heading</h1>
-  <p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei. Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.</p>-->
-  <button id="myBtn" onclick="myFunction()">Pause</button>
+  <p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei. Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.</p>
+  <button id="myBtn" onclick="myFunction()">Pause</button>-->
   <button id="myBtn">Contact Us</button>
 </div>
 
