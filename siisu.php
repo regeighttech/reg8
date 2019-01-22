e62ff8d45948404d6fa042fffa1f183c0c089858
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2019-01-22 00:09:19 -0600

    Adding siisu logo

--- siisu.php
+++ siisu.php
@@ -3,50 +3,56 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
-* {
-  box-sizing: border-box;
-}
+	* {
+	  box-sizing: border-box;
+	}
 
-body {
-  margin: 0;
-  font-family: Arial;
-  font-size: 17px;
-}
+	body {
+	  margin: 0;
+	  font-family: Arial;
+	  font-size: 17px;
+	}
 
-#myVideo {
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  min-width: 100%; 
-  min-height: 100%;
-}
+	#myVideo {
+	  position: fixed;
+	  right: 0;
+	  bottom: 0;
+	  min-width: 100%; 
+	  min-height: 100%;
+	}
 
-.content {
-  position: fixed;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.5);
-  color: #f1f1f1;
-  width: 100%;
-  padding: 20px;
-}
+	.content {
+	  position: fixed;
+	  bottom: 0;
+	  background: rgba(0, 0, 0, 0.5);
+	  color: #f1f1f1;
+	  width: 100%;
+	  padding: 20px;
+	}
 
-#myBtn {
-  width: 200px;
-  font-size: 18px;
-  padding: 10px;
-  border: none;
-  background: #000;
-  color: #fff;
-  cursor: pointer;
-}
+	#myBtn {
+	  width: 200px;
+	  font-size: 18px;
+	  padding: 10px;
+	  border: none;
+	  background: #000;
+	  color: #fff;
+	  cursor: pointer;
+	}
 
-#myBtn:hover {
-  background: #ddd;
-  color: black;
-}
+	#myBtn:hover {
+	  background: #ddd;
+	  color: black;
+	}
+	img{
+		position:absolute;
+		left:0;
+		top:0;
+	}
 </style>
 </head>
 <body>
+<img src="img/siisu.png" alt="SiiSu Logo">
 
 <video autoplay muted loop id="myVideo">
   <source src="city.mp4" type="video/mp4">
@@ -54,9 +60,10 @@ body {
 </video>
 
 <div class="content">
-  <h1>Heading</h1>
-  <p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei. Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.</p>
+  <!--<h1>Heading</h1>
+  <p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei. Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.</p>-->
   <button id="myBtn" onclick="myFunction()">Pause</button>
+  <button id="myBtn">Contact Us</button>
 </div>
 
 <script>
@@ -66,10 +73,10 @@ var btn = document.getElementById("myBtn");
 function myFunction() {
   if (video.paused) {
     video.play();
-    btn.innerHTML = "Pause";
+    //btn.innerHTML = "Pause";
   } else {
     video.pause();
-    btn.innerHTML = "Play";
+    //btn.innerHTML = "Play";
   }
 }
 </script>
