0503b3d4cdd594205da85a7690c867c73c3cee4e
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2019-01-22 19:58:36 -0600

    Adding new logo to siisu.php

--- siisu.php
+++ siisu.php
@@ -67,13 +67,13 @@
 </video>
 
 <!--<img src="img/siisu.png" alt="SiiSu Logo">-->
-<center><img src="img/siisu.png" alt="SiiSu Logo"></center>
+<img src="img/siisu.png" alt="SiiSu Logo">
 
 <div class="content">
   <!--<h1>Heading</h1>
   <p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei. Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.</p>
   <button id="myBtn" onclick="myFunction()">Pause</button>-->
-  <button id="myBtn">Contact Us</button>
+  <center><button id="myBtn">Contact Us</button></center>
 </div>
 
 <script>
