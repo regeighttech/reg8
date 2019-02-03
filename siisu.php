db8e8c5d42c9448c2fd865109de64df7880c0fe8
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2019-02-02 21:20:47 -0600

    Upgrading the quality of video background

--- siisu.php
+++ siisu.php
@@ -26,7 +26,6 @@
 		.content {
 		  position: fixed;
 		  bottom: 0;
-		  left:45%;
 		  background: rgba(0, 0, 0, 0.5);
 		  color: #f1f1f1;
 		  width: 100%;
@@ -42,6 +41,7 @@
 		  color: #fff;
 		  cursor: pointer;
 		  right: 41%;
+		  bottom: 0%;
 		  position: absolute;
 		}
 
