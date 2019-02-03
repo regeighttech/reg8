ba0b205964ca608db169648e7bc8ae4d45c566cc
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2019-02-02 21:47:17 -0600

    Contact us tab added

--- siisu.php
+++ siisu.php
@@ -186,7 +186,7 @@
 
 
 	</style>
-	<script type="text/javascript" src="./lib/jquery-3.3.1.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
 
