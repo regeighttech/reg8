901545867b2fc314c69594e604cc4796aa59ab6d
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2019-02-02 18:42:41 -0600

    Adding new SiiSu logos

--- siisu.php
+++ siisu.php
@@ -1,91 +1,112 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-	* {
-	  box-sizing: border-box;
-	}
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+		* {
+		  box-sizing: border-box;
+		}
 
-	body {
-	  margin: 0;
-	  font-family: Arial;
-	  font-size: 17px;
-	}
+		body {
+		  margin: 0;
+		  font-family: Arial;
+		  font-size: 17px;
+		}
 
-	#myVideo {
-	  position: fixed;
-	  right: 0;
-	  bottom: 0;
-	  min-width: 100%; 
-	  min-height: 100%;
-	}
+		#myVideo {
+		  position: fixed;
+		  right: 0;
+		  bottom: 0;
+		  min-width: 100%; 
+		  min-height: 100%;
+		}
 
-	.content {
-	  position: fixed;
-	  bottom: 0;
-	  background: rgba(0, 0, 0, 0.5);
-	  color: #f1f1f1;
-	  width: 100%;
-	  padding: 20px;
-	}
+		.content {
+		  position: fixed;
+		  bottom: 0;
+		  background: rgba(0, 0, 0, 0.5);
+		  color: #f1f1f1;
+		  width: 100%;
+		  padding: 20px;
+		}
 
-	#myBtn {
-	  width: 200px;
-	  font-size: 18px;
-	  padding: 10px;
-	  border: none;
-	  background: #000;
-	  color: #fff;
-	  cursor: pointer;
-	}
+		#myBtn {
+		  width: 200px;
+		  font-size: 18px;
+		  padding: 10px;
+		  border: none;
+		  background: #000;
+		  color: #fff;
+		  cursor: pointer;
+		}
 
-	#myBtn:hover {
-	  background: #ddd;
-	  color: black;
-	}
-	img{
-		position:fixed;
-	    max-width:20%;
-	  	max-height:20%;
-	  	top: 45%;
-		left: 40%;
-	}
+		#myBtn:hover {
+		  background: #ddd;
+		  color: black;
+		}
+
+		#center{
+			position:fixed;
+		    max-width:20%;
+		  	max-height:20%;
+		  	top: 45%;
+			left: 40%;
+		}
+
+		#topRight{
+			position:fixed;
+		    max-width:20%;
+		  	max-height:20%;
+		  	top: 0%;
+			right: 80%;
+		}
+
+		#topLeft{
+			position:fixed;
+		    max-width:20%;
+		  	max-height:20%;
+		  	top: 0%;
+			left: 0%;
+		}
 
 
-</style>
+	</style>
 </head>
 <body>
 
-<video autoplay muted loop id="myVideo">
-  <source src="city.mp4" type="video/mp4">
-  Your browser does not support HTML5 video.
-</video>
+	<video autoplay muted loop id="myVideo">
+	  <source src="city.mp4" type="video/mp4">
+	  Your browser does not support HTML5 video.
+	</video>
 
-<!--<img src="img/siisu.png" alt="SiiSu Logo">-->
-<img src="img/siisu.png" alt="SiiSu Logo">
+	<!--<img src="img/siisu.png" alt="SiiSu Logo">-->
+	<img id="center" src="img/siisu.png" alt="SiiSu Logo">
 
-<div class="content">
-  <!--<h1>Heading</h1>
-  <p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei. Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.</p>
-  <button id="myBtn" onclick="myFunction()">Pause</button>-->
-  <center><button id="myBtn">Contact Us</button></center>
-</div>
+	<img id="topRight" src="img/SiiSUBlueBlack.png" alt="SiiSu Logo">
 
-<script>
-var video = document.getElementById("myVideo");
-var btn = document.getElementById("myBtn");
+	<img id="topLeft" src="img/SiiSUBlueWhite.svg" alt="SiiSu Logo">
 
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    //btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    //btn.innerHTML = "Play";
-  }
-}
-</script>
+	<div class="content">
+	  <!--<h1>Heading</h1>
+	  <p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei. Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.</p>
+	  <button id="myBtn" onclick="myFunction()">Pause</button>-->
+	  <center><button id="myBtn">Contact Us</button></center>
+	</div>
+
+	<script>
+	var video = document.getElementById("myVideo");
+	var btn = document.getElementById("myBtn");
+
+	function myFunction() {
+	  if (video.paused) {
+	    video.play();
+	    //btn.innerHTML = "Pause";
+	  } else {
+	    video.pause();
+	    //btn.innerHTML = "Play";
+	  }
+	}
+	</script>
 
 </body>
 </html>
