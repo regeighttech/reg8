f514499d1b21705dc9b279cd0c85bc5894a9007c
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2019-02-04 16:06:49 -0600

    Functionality to send email added

--- contactFormHandler.php
+++ contactFormHandler.php
@@ -5,7 +5,7 @@
 		$mobile = $_POST['mobile'];
 		$notes = $_POST['notes'];
 
-		$to='brayannereyes12@gmail.com';
+		$to='br3psi@siisu.org';
 		$subject = 'Contact Us Form Submission';
 		$message = "Name: " . $name . "\n" . "Email: " . $email . "\n" . "Mobile: " . $mobile . "\n" . "Notes: " . $notes;
 		$headers = "From: " . $email;
