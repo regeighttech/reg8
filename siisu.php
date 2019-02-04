0d465bc14d27bcfe922f9dccf5a94378af032bd3
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2019-02-04 14:10:38 -0600

    Functionality to send email added

--- siisu.php
+++ siisu.php
@@ -208,11 +208,11 @@
 	</div>
 
 	<script>
-	var frmvalidator  = new Validator("contact_form");
+	/*var frmvalidator  = new Validator("contact_form");
 	frmvalidator.addValidation("name","req","Please provide your name");
 	frmvalidator.addValidation("email","req","Please provide your email");
 	frmvalidator.addValidation("email","email",
-	  "Please enter a valid email address");
+	  "Please enter a valid email address");*/
 
 
 	var video = document.getElementById("myVideo");
