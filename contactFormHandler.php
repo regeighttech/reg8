3e5a5ba9deba7688d923fbf6274f8f2ea61b4678
Jose Diaz <jodiaz@csumb.edu>
2019-02-05 21:11:53 -0800

    testing new lib

--- contactFormHandler.php
+++ contactFormHandler.php
@@ -18,31 +18,38 @@
 	// 	}
 
 	// }
- require_once "Mail.php";
  
- // $from = "Sandra Sender <sender@example.com>";
- // $to = "Ramona Recipient <recipient@example.com>";
- // $subject = "Hi!";
- // $body = "Hi,\n\nHow are you?";
- 
- // $host = "mail.gdom.net";
- // $username = "lfc hosted email";
- // $password = "email password";
- 
- // $headers = array ('From' => $from,
- //   'To' => $to,
- //   'Subject' => $subject);
- // $smtp = Mail::factory('smtp',
- //   array ('host' => $host,
- //     'auth' => true,
- //     'username' => $username,
- //     'password' => $password));
- 
- // $mail = $smtp->send($to, $headers, $body);
- 
- // if (PEAR::isError($mail)) {
- //   echo("<p>" . $mail->getMessage() . "</p>");
- //  } else {
- //   echo("<p>Message successfully sent!</p>");
- //  }
+require("PHPMailer/src/PHPMailer.php");
+
+$mail = new PHPMailer();
+
+// $mail->IsSMTP();                                      // set mailer to use SMTP
+// $mail->Host = "smtp.gmail.com";  // specify main and backup server
+// $mail->SMTPAuth = true;     // turn on SMTP authentication
+// $mail->Username = "jswan";  // SMTP username
+// $mail->Password = "secret"; // SMTP password
+
+// $mail->From = "from@example.com";
+// $mail->FromName = "Mailer";
+// $mail->AddAddress("josh@example.net", "Josh Adams");
+// $mail->AddAddress("ellen@example.com");                  // name is optional
+// $mail->AddReplyTo("info@example.com", "Information");
+
+// $mail->WordWrap = 50;                                 // set word wrap to 50 characters
+// $mail->AddAttachment("/var/tmp/file.tar.gz");         // add attachments
+// $mail->AddAttachment("/tmp/image.jpg", "new.jpg");    // optional name
+// $mail->IsHTML(true);                                  // set email format to HTML
+
+// $mail->Subject = "Here is the subject";
+// $mail->Body    = "This is the HTML message body in bold!";
+// $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+
+// if(!$mail->Send())
+// {
+//    echo "Message could not be sent.";
+//    echo "Mailer Error: " . $mail->ErrorInfo;
+//    exit;
+// }
+
+// echo "Message has been sent";
 ?>
