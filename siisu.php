bf9ce350bed7b6f1dd1337e534ad2fd364902008
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2019-02-08 17:17:56 -0600

    Changing contact us form

--- siisu.php
+++ siisu.php
@@ -1,8 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title>SiiSU - A Regulatory Solution</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="UTF-8">
+
+	<!--FOR CONTACT FORM-->
+	<!--===============================================================================================-->
+		<link rel="icon" type="image/png" href="ContactForm/images/icons/favicon.ico"/>
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="ContactForm/vendor/bootstrap/css/bootstrap.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="ContactForm/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="ContactForm/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="ContactForm/vendor/animate/animate.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="ContactForm/vendor/css-hamburgers/hamburgers.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="ContactForm/vendor/animsition/css/animsition.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="ContactForm/vendor/select2/select2.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="ContactForm/vendor/daterangepicker/daterangepicker.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="ContactForm/css/util.css">
+		<link rel="stylesheet" type="text/css" href="ContactForm/css/main.css">
+	<!--===============================================================================================-->
+
 	<style>
 		@import "https://fonts.googleapis.com/css?family=Raleway";
 		* { box-sizing: border-box; }
@@ -207,7 +233,53 @@
 	<div class="content">
 		<!-- Contact Us form-->
 		<div id="myBtn">Contact Us</div>
-		<div w3-include-html="ContactForm/index.html"></div>
+
+		<div class="wrap-contact100">
+			<div class="contact100-form-title" style="background-image: url(ContactForm/images/bg-01.jpg);">
+				<span class="contact100-form-title-1">
+					Contact Us
+				</span>
+
+				<span class="contact100-form-title-2">
+					Feel free to drop us a line below!
+				</span>
+			</div>
+
+			<form class="contact100-form validate-form">
+				<div class="wrap-input100 validate-input" data-validate="Name is required">
+					<span class="label-input100">Full Name:</span>
+					<input class="input100" type="text" name="name" placeholder="Enter full name">
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<span class="label-input100">Email:</span>
+					<input class="input100" type="text" name="email" placeholder="Enter email addess">
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="wrap-input100 validate-input" data-validate="Phone is required">
+					<span class="label-input100">Phone:</span>
+					<input class="input100" type="text" name="phone" placeholder="Enter phone number">
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="wrap-input100 validate-input" data-validate = "Message is required">
+					<span class="label-input100">Message:</span>
+					<textarea class="input100" name="message" placeholder="Your Comment..."></textarea>
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="container-contact100-form-btn">
+					<button class="contact100-form-btn">
+						<span>
+							Submit
+							<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
+						</span>
+					</button>
+				</div>
+			</form>
+		</div>
 		<!--
 		<div id="contactForm">
 		  <img id="contactUsImage" src="reg8/img/contactUs.png" alt="Contact Us image">
@@ -226,6 +298,38 @@
 		-->
 
 	</div>
+
+	<div id="dropDownSelect1"></div>
+
+	<!--===============================================================================================-->
+		<script src="ContactForm/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<!--===============================================================================================-->
+		<script src="ContactForm/vendor/animsition/js/animsition.min.js"></script>
+	<!--===============================================================================================-->
+		<script src="ContactForm/vendor/bootstrap/js/popper.js"></script>
+		<script src="ContactForm/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<!--===============================================================================================-->
+		<script src="ContactForm/vendor/select2/select2.min.js"></script>
+	<!--===============================================================================================-->
+		<script src="ContactForm/vendor/daterangepicker/moment.min.js"></script>
+		<script src="ContactForm/vendor/daterangepicker/daterangepicker.js"></script>
+	<!--===============================================================================================-->
+		<script src="ContactForm/vendor/countdowntime/countdowntime.js"></script>
+	<!--===============================================================================================-->
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAKFWBqlKAGCeS1rMVoaNlwyayu0e0YRes"></script>
+		<script src="js/map-custom.js"></script>
+	<!--===============================================================================================-->
+		<script src="ContactForm/js/main.js"></script>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-23581568-13');
+		</script>
 
 	<script>
 	var video = document.getElementById("myVideo");
