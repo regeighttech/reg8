0c7cf0c7e60f992eaa3a61b8efd78e1f472cfbe5
Jose Diaz <jodiaz@csumb.edu>
2019-02-09 20:48:11 -0800

    testing sending email

--- contactFormHandler.php
+++ contactFormHandler.php
@@ -27,29 +27,67 @@ require 'PHPMailer/src/SMTP.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 $mail = new PHPMailer();
-// $mail->IsSMTP();                                      // set mailer to use SMTP
-$mail->Host = "siisu.org";  // specify main and backup server
-$mail->SMTPAuth = true;     // turn on SMTP authentication
-$mail->Username = $hostEmail;  // SMTP username
-$mail->Password = $mailPass; // SMTP password
-$mail->SMTPSecure = 'tls';
-$mail->Port     = 587;
+
+//Enable SMTP debugging. 
+$mail->SMTPDebug = 3;                               
+//Set PHPMailer to use SMTP.
+$mail->isSMTP();            
+//Set SMTP host name                          
+$mail->Host = "smtp.gmail.com";
+//Set this to true if SMTP host requires authentication to send email
+$mail->SMTPAuth = true;                          
+//Provide username and password     
+$mail->Username = $hostEmail;                 
+$mail->Password = $mailPass                           
+//If SMTP requires TLS encryption then set it
+$mail->SMTPSecure = "tls";                           
+//Set TCP port to connect to 
+$mail->Port = 587;                                   
+
 $mail->From = $hostEmail;
 $mail->FromName = "Pedro Salinas Ibarra";
-$mail->AddAddress($recEmail);
-// $mail->AddReplyTo("info@example.com", "Information");
-$mail->WordWrap = 50;                                 // set word wrap to 50 characters
-// $mail->AddAttachment("/var/tmp/file.tar.gz");         // add attachments
-// $mail->AddAttachment("/tmp/image.jpg", "new.jpg");    // optional name
-$mail->IsHTML(true);                                  // set email format to HTML
-$mail->Subject = "";
-$mail->Body    = "This is the HTML researched";
-// $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
-if(!$mail->Send())
+
+$mail->addAddress($recEmail);
+
+$mail->isHTML(true);
+
+$mail->Subject = "Subject Text";
+$mail->Body = "<i>Mail body in HTML</i>";
+$mail->AltBody = "This is the plain text version of the email content";
+
+if(!$mail->send()) 
 {
-   echo "Message could not be sent.";
-   echo "Mailer Error: " . $mail->ErrorInfo;
-   exit;
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} 
+else 
+{
+    echo "Message has been sent successfully";
 }
-echo "Message has been sent";
+
+
+// // $mail->IsSMTP();                                      // set mailer to use SMTP
+// $mail->Host = "siisu.org";  // specify main and backup server
+// $mail->SMTPAuth = true;     // turn on SMTP authentication
+// $mail->Username = $hostEmail;  // SMTP username
+// $mail->Password = $mailPass; // SMTP password
+// $mail->SMTPSecure = 'tls';
+// $mail->Port     = 587;
+// $mail->From = $hostEmail;
+// $mail->FromName = "Pedro Salinas Ibarra";
+// $mail->AddAddress($recEmail);
+// // $mail->AddReplyTo("info@example.com", "Information");
+// $mail->WordWrap = 50;                                 // set word wrap to 50 characters
+// // $mail->AddAttachment("/var/tmp/file.tar.gz");         // add attachments
+// // $mail->AddAttachment("/tmp/image.jpg", "new.jpg");    // optional name
+// $mail->IsHTML(true);                                  // set email format to HTML
+// $mail->Subject = "";
+// $mail->Body    = "This is the HTML researched";
+// // $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+// if(!$mail->Send())
+// {
+//    echo "Message could not be sent.";
+//    echo "Mailer Error: " . $mail->ErrorInfo;
+//    exit;
+// }
+// echo "Message has been sent";
 ?>
