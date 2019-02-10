e21efd554564ab129ef7bd41a4b5a5e3ce270ca5
Jose Diaz <jodiaz@csumb.edu>
2019-02-09 20:37:31 -0800

    testing sending email

--- contactFormHandler.php
+++ contactFormHandler.php
@@ -1,21 +1,55 @@
 <?php
-	if(isset($_POST['submit'])){
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$phone = $_POST['phone'];
-		$notes = $_POST['notes'];
+	// if(isset($_POST['submit'])){
+	// 	$name = $_POST['name'];
+	// 	$email = $_POST['email'];
+	// 	$phone = $_POST['phone'];
+	// 	$notes = $_POST['notes'];
 
-		$to='br3psi@siisu.org';
-		$subject = 'Contact Us Form Submission';
-		$message = "Name: " . $name . "\n" . "Email: " . $email . "\n" . "Phone: " . $phone . "\n" . "Message: " . $notes;
-		$headers = "From: " . $email;
+	// 	$to='br3psi@siisu.org';
+	// 	$subject = 'Contact Us Form Submission';
+	// 	$message = "Name: " . $name . "\n" . "Email: " . $email . "\n" . "Phone: " . $phone . "\n" . "Message: " . $notes;
+	// 	$headers = "From: " . $email;
 
-		if(mail($to, $subject, $message, $headers)){
-			header("Location: ../index.php");
-		}
-		else{
-			echo "Something went wrong!";
-		}
+	// 	if(mail($to, $subject, $message, $headers)){
+	// 		header("Location: ../index.php");
+	// 	}
+	// 	else{
+	// 		echo "Something went wrong!";
+	// 	}
 
-	}
+	// }
+
+include "../keys/cred.php";
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+// require("PHPMailer/src/PHPMailer.php");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+$mail = new PHPMailer();
+// $mail->IsSMTP();                                      // set mailer to use SMTP
+$mail->Host = "siisu.org";  // specify main and backup server
+$mail->SMTPAuth = true;     // turn on SMTP authentication
+$mail->Username = $hostEmail;  // SMTP username
+$mail->Password = $mailPass; // SMTP password
+$mail->SMTPSecure = 'tls';
+$mail->Port     = 587;
+$mail->From = $hostEmail;
+$mail->FromName = "Pedro Salinas Ibarra";
+$mail->AddAddress($recEmail);
+// $mail->AddReplyTo("info@example.com", "Information");
+$mail->WordWrap = 50;                                 // set word wrap to 50 characters
+// $mail->AddAttachment("/var/tmp/file.tar.gz");         // add attachments
+// $mail->AddAttachment("/tmp/image.jpg", "new.jpg");    // optional name
+$mail->IsHTML(true);                                  // set email format to HTML
+$mail->Subject = "";
+$mail->Body    = "This is the HTML researched";
+// $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+if(!$mail->Send())
+{
+   echo "Message could not be sent.";
+   echo "Mailer Error: " . $mail->ErrorInfo;
+   exit;
+}
+echo "Message has been sent";
 ?>
