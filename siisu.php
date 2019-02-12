e5f03967f445358b64a65e0b091bf38210221a56
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2019-02-12 00:51:03 -0600

    Changing contact us form to be mobile friendly

--- siisu.php
+++ siisu.php
@@ -146,6 +146,7 @@
 							<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
 						</span>
 					</button>
+				</div>
 				<div class="container-contact100-form-btn" style="color:#ff4b5a; float:right;">
 					<button class="contact100-form-btn" >
 							<span>
@@ -153,7 +154,6 @@
 								<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
 							</span>
 					</button>
-				</div>
 				</div>
 			</form>
 		</div>
