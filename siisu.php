27bfa8a7b9399168d275ae3a093f3f26b005108b
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2019-02-28 18:33:13 -0600

    Added navigation bar to all pages

--- siisu.php
+++ siisu.php
@@ -34,10 +34,10 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="node_modules/bootstrap-social/bootstrap-social.css">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="reg8/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="reg8/node_modules/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="reg8/node_modules/bootstrap-social/bootstrap-social.css">
+    <link rel="stylesheet" href="reg8/css/styles.css">
 	<style type="text/css">
 
 		button:focus {outline:0 !important;}
@@ -263,8 +263,8 @@
 	});
 	</script>
     <!-- jQuery first, then Popper.js, then Bootstrap JS. -->
-    <script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
-    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
-    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="reg8/node_modules/jquery/dist/jquery.slim.min.js"></script>
+    <script src="reg8/node_modules/popper.js/dist/umd/popper.min.js"></script>
+    <script src="reg8/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
